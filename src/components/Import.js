1466b159388e6f245628ed78c8efaa64ac803e51
bwdurst <bwdurst@gmail.com>
2020-04-26 10:04:01 -0500

    project complete

--- src/components/Import.js
+++ src/components/Import.js
@@ -1,10 +1,49 @@
-import React from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import {
+    Container,
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableRow
+} from '@material-ui/core';
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+import Paper from '@material-ui/core/Paper';
+
 
 const Import = (props) => {
-    // fill out this component
+    const useStyles = makeStyles({
+        table: {
+            minWidth: 650,
+        },
+    });
+
 
     return (
-        <p>Import Component</p>
+        <div>
+            <h1>Import List of Car Makes</h1>
+            <h2>Count: {props.makes.length}</h2>
+            <Button variant="contained" color="primary" onClick={props.fetchMakes}>
+                Import
+            </Button>
+            <TableBody>
+                {props.makes.map(make => {
+                    return (
+                        <TableRow key={make.MakeId}>
+                            <TableCell>{make.MakeId}</TableCell>
+                            <TableCell>{make.MakeName}</TableCell>
+                            <TableCell>
+                                <Button onClick={() => props.removeMake(make.MakeId)}>
+                                    <DeleteForeverIcon />
+                                </Button>
+                            </TableCell>
+                        </TableRow>
+                    )
+                })}
+            </TableBody>
+        </div>
     )
 }
 
